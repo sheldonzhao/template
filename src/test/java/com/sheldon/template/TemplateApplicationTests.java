4ac5efc8657dab7ba9e5d2eede98880c56aa6b28
helloworld_zhao@163.com <helloworld_zhao@163.com>
2020-10-09 15:54:11 +0800

    init commit

---
+++ src/test/java/com/sheldon/template/TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheldon.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
